f4966907996f752204d3bc43e88a53172d7a23b3
saket bhushan <saket.bhushan46@gmail.com>
2020-07-28 13:13:37 +0530

    setup

---
+++ Documents/14PatternsOfCoding/14PatternsOfCoding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  14PatternsOfCoding
+//
+//  Created by saket bhushan on 28/07/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
